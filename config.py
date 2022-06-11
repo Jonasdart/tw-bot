09d50cb3c2102804e7dde2a5910ede0622c6de37
jonasdart <duarte.jsystem@gmail.com>
2022-06-11 02:32:59 -0300

    fix: Env getter

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-ACCESS_TOKEN = os.get("ACCESS_TOKEN")
-CHANNELS_NAME = os.get("CHANNELS_NAME", "").split(";")
-COMMANDS_PREFIX = os.get("COMMANDS_PREFIX")
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
+CHANNELS_NAME = os.environ.get("CHANNELS_NAME", "").split(";")
+COMMANDS_PREFIX = os.environ.get("COMMANDS_PREFIX")
